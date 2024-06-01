028fd3f6ef486dedbb864e5e6c7452d8f47d2225
Fayz <izzhanzy@gmail.com>
2024-06-01 20:45:18 +0700

    rollback

--- src/component/title.jsx
+++ src/component/title.jsx
@@ -4,16 +4,14 @@ function Title () {
             <div className="hero w-full flex justify-center items-center">
             <div className="overlay dark:bg-colos-primary bg-colos-accent bg-opacity-40 dark:bg-opacity-60">
                 <div className="flex justify-center items-center relative z-[999999]">
-                   <h1 className="text-white 2xl:text-4xl xl:text-3xl text-3xl text-center font-extrabold font-mono p-10 2xl:mt-20 xl:mt-7 mt-7 ">
+                   <h1 className="text-white 2xl:text-4xl xl:text-3xl text-3xl text-center font-extrabold font-mono p-10 2xl:mt-20 xl:mt-2 mt-7">
                      <span className="text-indigo-500 title">Hanif AI🍀</span>: Jawaban <span className="text-red-700">Cepat</span>, Ilmu <span className="text-yellow-400">Tepat</span>
                    </h1>
                 </div>
             </div>
             <div className="z-20">
-                <div className="fixed lg:relative flex justify-center items-center left-0 lg:top-[5rem] top-[7rem] bottom-0 right-0">
-                    <img src="/Image/logo.webp" alt="" className="2xl:w-[45rem] xl:w-[32rem]"/>
-                </div>
-               <h1 className="text-white text-2xl font-bold font-mono items-center text-center px-4 py-10 fixed lg:relative right-0 left-0 mt-32 lg:mt-0">Welcome To <a href="" className="underline italic">chathanif.vercel.app</a></h1>
+               <img src="/Image/logo.webp" alt="" className="2xl:w-[45rem] xl:w-[32rem]"/>
+               <h1 className="text-white text-2xl font-bold font-mono items-center text-center px-4">Welcome To <a href="" className="underline italic">chathanif.vercel.app</a></h1>
             </div>
           </div>
             <div className="p-10">
