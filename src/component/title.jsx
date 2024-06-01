9c4468e313f11034803c66b31f903414036785e2
Fayz <izzhanzy@gmail.com>
2024-06-01 20:15:43 +0700

    update UI

--- src/component/title.jsx
+++ src/component/title.jsx
@@ -1,7 +1,38 @@
 function Title () {
     return (
         <div className="">
-            <h1 className="text-white text-xl font-bold font-mono">Tanyakan apapun pada Hanif</h1>
+            <div className="hero w-full flex justify-center items-center">
+            <div className="overlay dark:bg-colos-primary bg-colos-accent bg-opacity-40 dark:bg-opacity-60">
+                <div className="flex justify-center items-center relative z-[999999]">
+                   <h1 className="text-white 2xl:text-4xl xl:text-3xl text-3xl text-center font-extrabold font-mono p-10 2xl:mt-20 xl:mt-2 mt-7">
+                     <span className="text-indigo-500 title">Hanif AI🍀</span>: Jawaban <span className="text-red-700">Cepat</span>, Ilmu <span className="text-yellow-400">Tepat</span>
+                   </h1>
+                </div>
+            </div>
+            <div className="z-20">
+               <img src="/Image/logo.webp" alt="" className="2xl:w-[45rem] xl:w-[32rem]"/>
+               <h1 className="text-white text-2xl font-bold font-mono items-center text-center">Welcome To <a href="chathanif.vercel.app" className="underline italic">chathanif.vercel.app</a></h1>
+            </div>
+          </div>
+            <div className="p-10">
+                <div className="py-6">
+                    <h3 className="text-white 2xl:text-xl">Situs chat AI terdepan yang siap membantu Anda menjawab pertanyaan seputar matematika, fisika, kimia, dan biologi dengan kecepatan dan akurasi yang tak tertandingi.</h3>
+                </div>
+
+                <div className="py-6">
+                    <h1 className="text-white text-xl 2xl:text-2xl font-bold font-mono mb-3">Jawaban Cepat, Akurat, dan Mudah untuk Setiap Pertanyaan Sains Anda!</h1>
+                    <h3 className="text-white 2xl:text-xl">Dibangun dengan teknologi mutakhir, AI kami dirancang untuk memberikan jawaban tepat dan cepat, memastikan Anda mendapatkan bantuan yang Anda butuhkan dalam waktu singkat.</h3>
+                </div>
+                
+                <h3 className="text-white 2xl:text-xl">Baik Anda seorang pelajar, profesional, atau sekadar penasaran, AI kami siap memberikan penjelasan yang jelas dan mendetail, membantu Anda memahami konsep-konsep kompleks dengan mudah.</h3>
+
+                <div className="py-6">
+                    <h1 className="text-indigo-500 title 2xl:text-3xl text-xl font-bold flex justify-center items-center py-5">Hanif AI🍀</h1>
+                    <h3 className="text-white 2xl:text-xl"><span>Di sini, </span> Anda akan menemukan jawaban cepat dan akurat untuk setiap pertanyaan sains Anda!</h3>
+                </div>
+            
+            </div>
+            
         </div>
     )
 }
